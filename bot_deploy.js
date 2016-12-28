e005fa37e3a12eebacd6c4f9ce705e11c30452bd
Kartikeya <kartvat3@in.ibm.com>
2016-12-28 17:44:18 +0530

    yo

--- bot_deploy.js
+++ bot_deploy.js
@@ -1,25 +1,33 @@
 var builder = require('botbuilder');
 var restify = require('restify');
 var greetings = require("./greetings.js");
-var dotenv = require('dotenv');
+// var dotenv = require('dotenv');
 
-//dotenv.load()
+// dotenv.load()
 
 //=========================================================
 // Bot Setup
 //=========================================================
 
 // Setup Restify Server
+
+
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
+
   
 // Create chat bot
+console.log(process.env.MICROSOFT_APP_ID)
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: '83a1a072-7173-464f-987c-3fa2b8c2d029',//process.env.MICROSOFT_APP_ID'',
+    appPassword: 'Tu1fLBTmxfL6c4qCrcsSbn6'
 });
+
+console.log(connector);
+
+console.log(connector.appId)
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
@@ -30,3 +38,6 @@ server.post('/api/messages', connector.listen());
 bot.dialog('/', function (session) {
     session.send("Hello World");
 });
+
+
+
